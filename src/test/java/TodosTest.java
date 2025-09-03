603168c2c27069cd795840a67d92a54db48a481f
EsquireRider <Esquirerider507@gmail.com>
2025-09-03 17:38:35 +0300

    Todos1

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -103,9 +103,78 @@ class TodosTest {
         todos.add(meeting);
 
         Task[] expected = {};
-        Task[] actual = todos.search("Сыр");
+        Task[] actual = todos.search("Пельмени");
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
+    @Test
+    public void shouldGetSubtasksEpic() {
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Assertions.assertArrayEquals(subtasks, epic.getSubtasks());
+    }
+
+    @Test
+    public void shouldGetMeeting() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+        Assertions.assertEquals("Выкатка 3й версии приложения", meeting.getTopic());
+        Assertions.assertEquals("Приложение НетоБанка", meeting.getProject());
+        Assertions.assertEquals("Во вторник после обеда", meeting.getStart());
+    }
+
+    @Test
+    public void shouldGetTitleSimpleTask() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        Assertions.assertEquals("Позвонить родителям", simpleTask.getTitle());
+    }
+
+    @Test
+    public void shouldGetTaskId() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+        Assertions.assertEquals(5, simpleTask.getId());
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+        Assertions.assertEquals(55, epic.getId());
+
+        Meeting meeting = new Meeting(555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда");
+        Assertions.assertEquals(555, meeting.getId());
+    }
+
+    @Test
+    public void shouldEqualsAndHashCode() {
+        SimpleTask task1 = new SimpleTask(55, "SimpleTask");
+        SimpleTask task2 = new SimpleTask(55, "Epic");
+        SimpleTask task3 = new SimpleTask(555, "Meeting");
+
+        Assertions.assertEquals(task1, task2);
+        Assertions.assertEquals(task1.hashCode(), task2.hashCode());
+
+        Assertions.assertNotEquals(task1, task3);
+        Assertions.assertNotEquals(task1.hashCode(), task3.hashCode());
+
+        Assertions.assertEquals(true, task1.equals(task1));
+        Assertions.assertEquals(false, task1.equals(null));
+
+        Object object = new Object();
+        Assertions.assertEquals(false, task1.equals(object));
+    }
+
+    @Test
+    public void shouldTaskMatches() {
+        Task task = new Task(10);
+
+        Assertions.assertEquals(false, task.matches("Позвонить"));
+    }
 }
